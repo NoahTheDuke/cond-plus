b9da264d4d5ab3c2c330bfdd784dca75368259be
Noah Bogart <nbogart@batterii.com>
2020-03-23 14:07:41 -0400

    Allow keywords, update tests, reindent

--- src/cond_plus/core.clj
+++ src/cond_plus/core.clj
@@ -1,39 +1,44 @@
 (ns cond-plus.core)
 
 (defmacro cond+
-  [& clauses]
+  "Each test-expr is evaluated one at a time. If the test-expr returns logical
+  true (or is :else), the form is evaluated as described below and no further
+  cond-clauses are evaluated. (cond+) returns nil."
+  [& body]
   (letfn
-   [(cond-loop
-      [tests]
-      (let [line (first tests)
-            others (next tests)]
-        (when (some? line)
-          (if-not (or (list? line)
-                      (vector? line))
-            (throw (IllegalArgumentException. "clause is not in a vector"))
-            (let [test (first line)
-                  value (next line)]
-              (if (= 'else test)
-                (if (seq others)
-                  (throw (IllegalArgumentException. ":else not last"))
-                  (if (nil? value)
-                    (throw (IllegalArgumentException. "missing expression in else clause"))
-                    `(let [] ~@value)))
-                (if (= '=> (first value))
-                  (if (= 2 (count value))
-                    (let [exp (cond-loop others)
-                          gen (gensym)]
-                      `(let [~gen ~test]
-                         (if ~gen
-                           (~(second value) ~gen)
-                           ~exp)))
-                    (throw (IllegalArgumentException. "bad => clause")))
-                  (let [exp (cond-loop others)]
-                    (if (nil? value)
-                      (let [gen (gensym)]
-                        `(let [~gen ~test]
-                           (if ~gen ~gen ~exp)))
-                      `(if ~test
-                         (let [] ~@value)
-                         ~exp))))))))))]
-    (cond-loop clauses)))
+    [(cond-loop
+       [tests]
+       (when (seq tests)
+         (let [line (first tests)
+               others (next tests)]
+           (if-not (or (list? line)
+                       (vector? line))
+             (throw (IllegalArgumentException. "clause is not in a vector"))
+             (let [test (first line)
+                   value (next line)]
+               (if (or (= 'else test)
+                       (= :else test))
+                 (if (seq others)
+                   (throw (IllegalArgumentException. ":else not last"))
+                   (if (nil? value)
+                     (throw (IllegalArgumentException. "missing expression in else clause"))
+                     `(let [] ~@value)))
+                 (if (or (= '=> (first value))
+                         (= :> (first value)))
+                   (if (= 2 (count value))
+                     (let [exp (cond-loop others)
+                           gen (gensym)]
+                       `(let [~gen ~test]
+                          (if ~gen
+                            (~(second value) ~gen)
+                            ~exp)))
+                     (throw (IllegalArgumentException. "bad => clause")))
+                   (let [exp (cond-loop others)]
+                     (if (nil? value)
+                       (let [gen (gensym)]
+                         `(let [~gen ~test]
+                            (if ~gen ~gen ~exp)))
+                       `(if ~test
+                          (let [] ~@value)
+                          ~exp))))))))))]
+    (cond-loop body)))
--- test/cond_plus/core_test.clj
+++ test/cond_plus/core_test.clj
@@ -5,101 +5,114 @@
 (deftest cond+-test
   (testing "returns nil when given no input"
     (is (nil? (cond+))))
-  (testing "returns nil when clauses are empty or any test is nil (before a true test is found)"
-    (is (nil? (cond+ nil)))
+  (testing "returns nil when clauses are empty"
     (is (nil? (cond+ [])))
-    (is (nil? (cond+ [nil])))
-    (is (nil? (cond+ nil [true true])))
-    (is (nil? (cond+ [false true] nil))))
-  (testing "returns test if no expr given"
-    (is (= [1 2 3] (cond+ [(seq [1 2 3])])))
-    (is (= 1 (cond+ [1])))
-    (is (= 1 (cond+ [(first [1])])))
-    (is (= 1 (cond+ [false] [1]))))
-  (testing "returns first expr when associated test is true"
+    (is (nil? (cond+ [] []))))
+  (testing "returns first body when associated test is true"
     (is (= :first (cond+ [true :first])))
     (is (= :first (cond+
-                   [true :first]
-                   [true :second]))))
+                    [true :first]
+                    [true :second]))))
   (testing "returns nil when all tests are false"
     (is (nil? (cond+ [false :first]))))
-  (testing "returns expr when non-inital test is true"
-    (is (= :second
+  (testing "returns body when non-inital test is true"
+    (is (= :third
            (cond+
-            [false :first]
-            [true :second]))))
-  (testing "returns expr when non-inital test is true"
+             [false :first]
+             [nil :second]
+             [true :third]))))
+  (testing "returns body when non-inital test is true"
     (is (= :equal
            (cond+
-            [(< 3 3) :greater]
-            [(> 3 3) :less]
-            [(= 3 3) :equal]))))
-  (testing "don't run exprs of non-truthy branches"
+             [(< 3 3) :greater]
+             [(> 3 3) :less]
+             [(= 3 3) :equal]))))
+  (testing "don't run body of non-truthy branches"
     (is (let [view (atom 0)]
           (cond+
-           [false (swap! view inc)]
-           [true :first])
+            [false (swap! view inc)]
+            [true :first])
           (zero? @view))))
-  (testing "runs exprs of first truthy branch"
+  (testing "evaluates body of first truthy branch"
     (let [view (atom 0)]
       (is (= 1 (cond+
-                [true (swap! view inc)]
-                [true :first])))
+                 [true (swap! view inc)]
+                 [true :first])))
       (is (= 1 @view))))
-  (testing "exprs have an implicit do"
+  (testing "evaluation of body has an implicit do"
     (is (= :second (cond+ [(< 1 2) :first :second])))
     (is (= :second
            (cond+
-            [(> 1 2) :ok]
-            [(= 1 1) :first :second])))
+             [(> 1 2) :ok]
+             [(= 1 1) :first :second])))
     (let [view (atom 0)]
       (is (= :second
              (cond+
-              [(> 1 2) :ok]
-              [(= 1 1) (swap! view inc) :second])))
+               [(> 1 2) :ok]
+               [(= 1 1) (swap! view inc) :second])))
       (is (= 1 @view))))
   (testing "branches can use vectors or lists"
     (is (= :equal
            (cond+
-            ((< 3 3) :greater)
-            ((> 3 3) :less)
-            ((= 3 3) :equal)))))
+             ((< 3 3) :greater)
+             ((> 3 3) :less)
+             ((= 3 3) :equal)))))
+  (testing "=>"
+    (testing "calls given function on result of first true test"
+      (is (= 1 (cond+ [[1 2 3] :> first])))
+      (is (= 2 (cond+ [(next [1 2 3]) :> first])))
+      (is (= [-2 -3] (cond+ [(next [1 2 3]) :> (fn [x] (map - x))])))
+      (is (= 2 (cond+ [(:key {:key 1}) :> inc])))
+      (is (= 2 (cond+
+                 [(:key {:key 1}) :> inc]
+                 [true true])))
+      (is (= 2 (cond+
+                 [false 7]
+                 [1 :> inc]))))
+    (testing "doesn't call given function when test returns false value"
+      (is (= false (cond+
+                     [(:miss {:key 1}) :> inc]
+                     [true false]))))
+    (testing "accepts symbol form"
+      (is (= 1 (cond+ [[1 2 3] => first])))))
+  (testing "no body given"
+    (testing "returns first true test"
+      (is (= [1 2 3] (cond+ [(seq [1 2 3])])))
+      (is (= 1 (cond+ [1])))
+      (is (= 1 (cond+ [(first [1])])))
+      (is (= 1 (cond+ [false] [1]))))
+    (testing "always evaluates body"
+      (let [view (atom true)]
+        (is (= 1 (cond+
+                   [(swap! view not)]
+                   [1])))
+        (is (false? @view)))))
   (testing "else"
-    (testing "always returns expr"
-      (is (= :first (cond+ [else :first]))))
-    (testing "returns expr when other branches are falsey"
+    (testing "always returns body"
+      (is (= :first (cond+ [:else :first]))))
+    (testing "returns body when other branches are falsey"
       (is (= :equal
              (cond+
-              [(< 3 3) :greater]
-              [(> 3 3) :less]
-              [else :equal]))))
-    (testing "exprs have an implicit do"
-      (is (= :second (cond+ [else :first :second])))
+               [(< 3 3) :greater]
+               [(> 3 3) :less]
+               [:else :equal]))))
+    (testing "evaluation of body has an implicit do"
+      (is (= :second (cond+ [:else :first :second])))
       (is (= :second
              (cond+
-              [(< 3 3) :greater]
-              [(> 3 3) :less]
-              [else :equal :second])))
+               [(< 3 3) :greater]
+               [(> 3 3) :less]
+               [:else :equal :second])))
       (let [view (atom 0)]
         (is (= :second
                (cond+
-                [(> 1 2) :ok]
-                [else (swap! view inc) :second])))
-        (is (= 1 @view)))))
-  (testing "=>"
-    (testing "calls given function on result of first true test"
-      (is (= 1 (cond+ [[1 2 3] => first])))
-      (is (= 2 (cond+ [(next [1 2 3]) => first])))
-      (is (= [-2 -3] (cond+ [(next [1 2 3]) => (fn [x] (map - x))])))
-      (is (= 2 (cond+ [(:key {:key 1}) => inc])))
-      (is (= 2 (cond+ [false 7] [1 => inc])))
-      )
-    (testing "doesn't call given function when test returns false value"
-      (is (= false (cond+
-                    [(:miss {:key 1}) => inc]
-                    [true false]))))))
+                 [false :ok]
+                 [:else (swap! view inc) :second])))
+        (is (= 1 @view))))
+    (testing "accepts symbol form"
+      (is (= :first (cond+ [else :first]))))))
 
-;; Borrowed from Clojure Core
+;; Adapted from Clojure Core
 (defn maintains-identity [f]
   (are [x] (= (f x) x)
       nil
@@ -154,4 +167,4 @@
     (cond+ [(< 3 2) (+ 1 2)] [true :result] [true (throw (new Exception "Exception which should never occur"))]) :result)
 
   ; identity (= (cond+ [true x]) x)
-  (maintains-identity (fn [_] (cond+ [true _]))))
+  (maintains-identity (fn [x] (cond+ [true x]))))
