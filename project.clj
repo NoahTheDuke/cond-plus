9bb4d999eb80102422518ba51927b9791ef1066a
Noah Bogart <nbogart@batterii.com>
2020-03-23 14:17:04 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cond-plus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cond-plus "1.0.0"
+  :description "An adaption of the Racket cond macro for Clojure."
+  :url "https://github.com/NoahTheDuke/cond-plus"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
