7689bc41a0b5b403abc385ff4acc9926a94b3dbd
Noah Bogart <nbogart@batterii.com>
2021-02-25 14:53:35 -0500

    Update to 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cond-plus "1.0.1"
+(defproject cond-plus "1.1.0"
   :description "An adaption of the Racket cond macro for Clojure."
   :url "https://github.com/NoahTheDuke/cond-plus"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
